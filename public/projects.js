45bee5544fd327c7b6cae8e6b31b663d1fa812c4
selçuk güler <2003022022@student.beykent.edu.tr>
2022-09-03 11:56:56 +0300

    added Password Strength Checker to readme and projects

--- public/projects.js
+++ public/projects.js
@@ -4,6 +4,7 @@ const projects = [
 	{id: 3, name: 'Random Password', link: '/random-password'},
 	{id: 4, name: 'Customer Feedback', link: '/customer-feedback'},
 	{id: 5, name: 'Tic Tac Toe', link: '/tic-tac-toe'},
+	{id: 6, name: 'Password Strength', link: '/password-strength'},
 ]
 
 export default projects;
